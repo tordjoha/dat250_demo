da8e3c48d98342d823647976687961ff7add9724
Tord Johansson <tordjoha1@gmail.com>
2025-08-27 11:26:46 +0200

    made hello a subpage

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,21 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
-
 	public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 	}
-
-    @RestController
-    static class HelloController {
-        @GetMapping("/")
-        public String hello() {
-            return "Hello World";
-        }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "me", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
