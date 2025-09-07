7973b02e4666178a6748f0d53cad73c7137789a9
Tord Johansson <tordjoha1@gmail.com>
2025-09-08 01:13:19 +0200

    fixed declaration

--- src/main/java/com/example/demo/dm/PollManager.java
+++ src/main/java/com/example/demo/dm/PollManager.java
@@ -10,19 +10,19 @@ public class PollManager {
     private final Map<String, User> users = new HashMap<>();
     private final Map<String, Poll> polls = new HashMap<>();
 
-    public void getUser(String username) {
+    public User getUser(String username) {
         users.get(username);
     }
 
-    public void addUser(User user) {
+    public User addUser(User user) {
         users.put(user.getUsername(), user);
     }
 
-    public void getPoll(String question) {
+    public Poll getPoll(String question) {
         polls.get(question);
     }
 
-    public void addPoll(Poll poll) {
+    public Poll addPoll(Poll poll) {
         polls.put(poll.getQuestion(), poll);
     }
 }
