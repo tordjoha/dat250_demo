59aa7fa8591ba18c58b3beaa3be963080050ba45
Tord Johansson <tordjoha1@gmail.com>
2025-09-14 14:10:57 +0200

    updated controller to working get mappings

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,24 +3,28 @@ package com.example.demo.controllers;
 import com.example.demo.dm.PollManager;
 import com.example.demo.dm.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     private final PollManager pollManager;
 
-    @Autowired
-    public UserController(PollManager pollManager)
-    {
+    public UserController(PollManager pollManager) {
         this.pollManager = pollManager;
     }
 
     @PostMapping
-    public void createUser(@RequestBody User user){
-        pollManager.addUser(user);
+    public User createUser(@RequestBody User user) {
+        return pollManager.addUser(user);
     }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User user = pollManager.getUser(username);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
 }
