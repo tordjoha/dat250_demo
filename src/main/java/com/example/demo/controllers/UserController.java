46bcfba45ba06dd42713d1ff53683594bc823d74
Tord Johansson <tordjoha1@gmail.com>
2025-09-15 00:03:52 +0200

    removed dead code

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.example.demo.controllers;
 
 import com.example.demo.dm.PollManager;
 import com.example.demo.dm.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
