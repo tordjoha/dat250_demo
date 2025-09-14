adb924b493220d09d1e7f5e814fc36f27a3c5148
Tord Johansson <tordjoha1@gmail.com>
2025-09-15 00:03:23 +0200

    added CORS server side

--- src/main/java/com/example/demo/controllers/PollController.java
+++ src/main/java/com/example/demo/controllers/PollController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/polls")
+@CrossOrigin
 public class PollController {
 
     private final PollManager pollManager;
--- src/main/java/com/example/demo/controllers/VoteController.java
+++ src/main/java/com/example/demo/controllers/VoteController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/polls/{pollId}/vote")
+@CrossOrigin
 public class VoteController {
 
     private final PollManager pollManager;
